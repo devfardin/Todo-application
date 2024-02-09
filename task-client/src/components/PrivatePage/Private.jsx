7fdf4db4ac216bd79e753aa2588b32c67185823e
fardin-ahmed-1 <contactfardin22@gmail.com>
2024-02-09 16:06:09 +0600

    update

---
+++ task-client/src/components/PrivatePage/Private.jsx
@@ -0,0 +1,18 @@
+import { Navigate, useLocation } from "react-router-dom"
+import useAuth from "../../hooks/useAuth"
+const Private = ({ children }) => {
+    const { user, loading } = useAuth()
+    const location =useLocation()
+    if (loading) {
+        return <div className="flex justify-center items-center h-screen">
+            <span className="loading loading-dots loading-lg text-primery"></span>
+        </div>
+    }
+    if (user) {
+        return children
+    }
+    return (
+        <Navigate state={location.pathname} to='/login'></Navigate>
+    )
+}
+export default Private
